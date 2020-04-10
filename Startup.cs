f4a52d3854051f6d20e540a723d27cea2f1d84f2
STEVE1601 <stephen.16.rmd@gmail.com>
2020-04-10 14:20:56 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace DotNetCoreSqlDb
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+                    options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
